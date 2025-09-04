ed36e3dd81ffe3db8ca768dbdb88e2314beb23a9
R.Amrutha <ammuamrutha26661@gmail.com>
2025-09-04 21:23:13 +0530

    right angle triangle pattern (left-aligned)
    prints right angle triangle pattern (left-aligned)

---
+++ main (16).c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,spaces,stars,n;
+	printf("enter n:");
+	scanf("%d",&n);
+	stars=1;
+	spaces=n-1;
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=spaces;j++)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=stars;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+		stars++;
+		spaces--;
+	}
+	return 0;
+}
