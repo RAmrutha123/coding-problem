e91ab3d9ebfaa301f06d1fcfdacd27c40297eaf5
R.Amrutha <ammuamrutha26661@gmail.com>
2025-09-11 21:48:10 +0530

    prime number or not
    checks whether the number is prime or not

---
+++ main (32).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,cnt=0;
+	printf("enter n:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		if(n%i==0)
+		   cnt++;
+	}
+	if(cnt==2)
+	  printf("prime");
+	else
+	  printf("not prime");
+  return 0;
+}
